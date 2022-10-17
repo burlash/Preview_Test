99561550f4259ba6777ba09da2816425e29bc07f
burlash <112091667+burlash@users.noreply.github.com>
2022-10-17 07:07:59 -0400

    token updated

--- AddUser.sh
+++ AddUser.sh
@@ -5,11 +5,10 @@ OWNER="geochar"
 REPO="arc-basic-java-tpl"
 USERNAME="burlash"
 
-echo "script started"
 
 curl \
   -X PUT \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${GH_TOKEN}" \
+  -H "Authorization: Bearer ${{secrets.GITHUB_TOKEN}}" \
   https://api.github.com/repos/${OWNER}/${REPO}/collaborators/${USERNAME} \
   -d "{"permission":"maintainer"}"
